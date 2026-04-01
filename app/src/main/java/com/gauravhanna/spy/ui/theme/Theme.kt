d562719c7490b7a3e0df016399176d23c101b0eb
Gaura <awadhvastram@gmail.com>
2026-04-01 18:48:54 +0530

    Frontend upgrade: Matrix background, sound effect, Render API_BASE

--- app/src/main/java/com/gauravhanna/spy/ui/theme/Theme.kt
+++ app/src/main/java/com/gauravhanna/spy/ui/theme/Theme.kt
@@ -34,7 +34,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun ServiceapkTheme(
+fun SpyTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
